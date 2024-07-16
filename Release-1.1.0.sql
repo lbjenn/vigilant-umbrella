a835721a3dcbc39349fd04e8f0d6c541e6c73ee7
lbjenn <jlewis@liquibase.com>
2024-07-16 14:13:12 -0400

    Added Release-1.1.0.sql so users don't have to cut and paste

---
+++ Release-1.1.0.sql
@@ -0,0 +1,21 @@
+--liquibase formatted sql
+--changeset jenn:20240402-create-T_student-table labels:jira-219,r1 
+--comment: create T_student table
+CREATE TABLE T_student(
+  student_fname VARCHAR(50),
+  student_lname VARCHAR(50),
+  state CHAR(2),
+  notes VARCHAR (355)
+);
+--rollback DROP TABLE T_student;
+
+--changeset jenn:20240402-insert-a-row-student1 labels:jira-219,r1 context:dev
+--comment: Add Viktor record into student table
+INSERT INTO T_student (student_fname, student_lname, state, testdml_rep,notes)
+VALUES ('Viktor', 'Hargreeves', 'NY', 'Random comment for notes');
+
+--changeset jenn:20240402-delete-a-row-student1 labels:jira-220,r1 context:dev
+--comment: remove Jenn from student table
+DELETE FROM T_student
+--rollback INSERT INTO T_student (student_fname, student_lname, state, testdml_rep,notes)
+--rollback VALUES ('Viktor', 'Hargreeves', 'NY', 'Random comment for notes'); 
